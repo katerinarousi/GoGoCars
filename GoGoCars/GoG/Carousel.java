120ca4c7f995df4fb7d686010ee788f6f29b6713
Katerina Rousi <t8210130@aueb.gr>
2023-11-27 13:52:03 +0200

    new servlet file

--- GoGoCars/GoG/Carousel.java
+++ GoGoCars/GoG/Carousel.java
@@ -1,7 +1,9 @@
 package GoG;
 import java.io.*;
+import javax.servlet.*;
 import javax.servlet.http.*;
+import java.sql.*;
 
-public class Carousel extends HttpServlet {
+public class Carousel extends HttpServlet{
     
 }
