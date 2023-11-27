943083115107ac8d8a419559a357cabf9a6ecdb6
t8210103 <115114218+t8210103@users.noreply.github.com>
2023-11-27 15:31:43 +0200

    added filter by price

--- GoGoCars/GoG/Carousel.java
+++
@@ -1,9 +0,0 @@
-package GoG;
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.sql.*;
-
-public class Carousel extends HttpServlet{
-    
-}
---
+++ GoGoCars/GoG/priceFilter.java
@@ -0,0 +1,8 @@
+package GoG;
+import java.util.List;
+
+public class priceFilter {
+    public List<Car> getPrices() throws Exception {
+        
+    }
+}
