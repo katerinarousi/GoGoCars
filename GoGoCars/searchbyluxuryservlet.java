e2e12f4c8dd4af9c6e1d6b11982f97c2e970c7cc
Kyriaki Parastatidou <parastkiki@hotmail.com>
2023-12-08 14:47:45 +0200

    trying to make the servlet work

--- GoGoCars/searchbyluxuryservlet.java
+++ GoGoCars/searchbyluxuryservlet.java
@@ -1,15 +1,25 @@
-import javax.servlet.http.*;
-import javax.servlet.*;
 import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
 
 
 
 public class searchbyluxuryservlet extends HttpServlet {
+   
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
         String location = request.getParameter("location");
         String pick_up = request.getParameter("pick_up");
         String drop_off = request.getParameter("drop_off");
+
+        System.out.println("Servlet: pick_up=" + pick_up + ", drop_off=" + drop_off);
+
+    
 
         if (pick_up==null || location==null || drop_off==null) {
             request.setAttribute("message", "Fill all the fields");
@@ -20,11 +30,11 @@ public class searchbyluxuryservlet extends HttpServlet {
 
         } else {
 
-            request.setAttribute("pick_up", pick_up);
-            request.setAttribute("drop_off", drop_off);
-
-            response.sendRedirect ("results.jsp");
+            RequestDispatcher dispatcher1 = request.getRequestDispatcher("results.jsp");
+            dispatcher1.forward(request, response);
         }
+
+    
 
     }
 }
