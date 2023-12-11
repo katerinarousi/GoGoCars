7eb452eb673555227ba17dba4efe4f5d527691f3
zoispyridi <zoe.spyr2@gmail.com>
2023-12-11 12:13:07 +0200

    c

--- GoGoCars/GoG/searchbyluxuryservlet.java
+++ GoGoCars/GoG/searchbyluxuryservlet.java
@@ -1,36 +1,19 @@
 package GoG;
 
-import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-
-
-
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.sql.*;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-
-
-
 
 
 public class searchbyluxuryservlet extends HttpServlet {
    
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-
-
-
-        String location = request.getParameter("location");
         String pick_up = request.getParameter("pick_up");
         String drop_off = request.getParameter("drop_off");
 
-     
       try{
     
           
